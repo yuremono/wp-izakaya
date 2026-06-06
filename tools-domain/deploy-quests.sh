f8b95650cb1cd7838a0bf0e2578f7fce6fdfa2fe
yuremono <5alviaofficinali50@gmail.com>
2026-06-06 14:34:00 +0900

    Fix quests deploy helper path

--- tools-domain/deploy-quests.sh
+++ tools-domain/deploy-quests.sh
@@ -7,4 +7,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export DEPLOY_THEME_SLUG="${DEPLOY_THEME_SLUG:-quests}"
 export DEPLOY_ZIP_NAME="${DEPLOY_ZIP_NAME:-quests-theme.zip}"
 
-exec "${SCRIPT_DIR}/../deploy.sh" "$@"
+exec "${SCRIPT_DIR}/../tools/deploy.sh" "$@"
